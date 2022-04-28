c2ae945dba9da3116d4d648ade3c46ae5bbad03a
Anthonia Udoba <99968773+AnthoniaUdoba@users.noreply.github.com>
2022-04-28 15:09:11 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,31 +3,29 @@ const colorPicker = document.getElementById('colorPicker');
 // Select size input by getting elements from HTML
 const sizePicker = document.getElementById('sizePicker');
 
+// When size is submitted by the user, call makeGrid()
+const canvas = document.getElementById('pixelCanvas');
+sizePicker.addEventListener('submit', function(submit) {
+   submit.preventDefault();
+   makeGrid();
+});
 // defining the makegrid function()
 function makeGrid() {
-  const width = document.getElementById('inputWidth').value;
   const height = document.getElementById('inputHeight').value;
-  const canvas = document.getElementById('pixelCanvas');
+  const width = document.getElementById('inputWidth').value;
   canvas.innerHTML = '';
   
 //creates tables and rows
   for (let column = 0; column < height; column++) {
-    let row = document.querySelector("#pixelCanvas").insertRow();
+    let row = document.getElementById("#canvas").insertRow();
     for (let r = 0; r< width; r++){
       row.insertCell();
     }
   }
-  
-// When size is submitted by the user, call makeGrid()
-  sizePicker.addEventListener('submit', function(submit) {
-  submit.preventDefault();
-  makeGrid(row, column);
-});
-
-// Add color to selected cell
+// Add color to the selected cell
   canvas.addEventListener('click', function(changecolor) {
     let color = colorPicker.value;
     console.log("Color:", color);
     changecolor.target.style.backgroundColor = color;
   });
-});
+};
